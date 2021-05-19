aa8ab964dc30ebbf19367af6a32c180d1a588950
Jason-Ling <1933924887@qq.com>
2021-05-19 22:51:12 +0800

    人脸编号改为字符串

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ is_save_face = int(input("是否需要保存图片[0-False, 1-True]："))
 path = "face_images/"
 origin_count, total_count = 0, 0
 if is_save_face:
-    face_id = int(input("人脸编号："))
+    face_id = str(input("人脸编号："))
     face_count = int(input("人脸数量："))
     path = path + str(face_id)
     origin_count = tools.mkdir(path)
@@ -38,17 +38,14 @@ while cap.isOpened():
                 fac_gray = gray[y: (y + h), x: (x + w)]
                 origin_count += 1
                 cv2.imwrite(path + '/' + str(origin_count) + '.jpg', fac_gray)
-
     cv2.putText(frame, "face number " + str(len(faces)), (10, 30), font, 1.0, (0, 255, 255), 1)
     cv2.imshow("Face Detect", frame)
     k = cv2.waitKey(10)
     if origin_count >= total_count:
         if is_save_face:
-
             print("图片保存完成")
         is_save_face = False
     if k == 27:
         break
-    # if k == ord('d'): 人脸识别
 cap.release()
 cv2.destroyAllWindows()
