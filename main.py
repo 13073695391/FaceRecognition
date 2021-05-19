00dc4f74f0416b674730afc4bf05daa655dd0859
Jason-Ling <1933924887@qq.com>
2021-05-19 20:57:44 +0800

    修改人脸检测

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import tensorflow as tf
 import cv2
 import dlib
-import numpy as np
-import face
 
-if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)
-    cap.set(3, 480)
-    # height = cap.get(cv2.CAP_PROP_XI_HEIGHT)
-    # width = cap.get(cv2.CAP_PROP_XI_WIDTH)
-    while cap.isOpened():
-        ret_flag, frame = cap.read()
-        detector = dlib.get_frontal_face_detector()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = detector(gray)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        for face in faces:
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
-        cv2.putText(frame, "faces: " + str(len(faces)), (20, 50), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
-        cv2.imshow("Face Detect", cv2.flip(frame, 1))
-        k = cv2.waitKey(1)
-        if k == ord('q') or k == 27:
-            break
-        # if k == ord('s'): 保存人脸
-        # if k == ord('d'): 人脸识别
-    cap.release()
-    cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier(r'D:\Program\ProgramData\Anaconda3\envs\tensorflow\Lib\site-packages\cv2\data'
+                                    r'\haarcascade_frontalface_default.xml')
+while cap.isOpened():
+    ret_flag, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 人脸检测
+    faces = faceCascade.detectMultiScale(
+        gray,  # 灰度图，使用灰度图会提高效率
+        scaleFactor=1.05,  # 图片缩小的值
+        minNeighbors=20,  # 判断次数
+        minSize=(72, 72)  # 人脸最小尺寸
+    )
+    for (x, y, w, h) in faces:
+        # 画脸的方框
+        # 图像源，原点，终点，线的颜色，粗细
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(frame, "face number:" + str(len(faces)), (10, 30), font, 1.0, (0, 255, 255), 1)
+    cv2.imshow("Face Detect", frame)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+    #保存人脸
+    # if k == ord('s'):
+
+    # if k == ord('d'): 人脸识别
+cap.release()
+cv2.destroyAllWindows()
